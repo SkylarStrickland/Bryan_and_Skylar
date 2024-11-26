0fac6540da3f18d2583201203b65b1df937653b9
SkylarStrickland <skylarpstrickland@gmail.com>
2024-11-25 21:15:24 -0500

    Add files via upload

---
+++ WebDevelopmentLab03/info.py
@@ -0,0 +1,71 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/IMG.jpg"
+about_me = "I'm Skylar Strickland and I'm from Dacula, GA. I'm a first-year student at Georgia tech and I enjoying hanging out with my friends. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/skylarpstrickland/"
+my_github_url = "https://github.com/SkylarStrickland"
+my_email_address = "skylarpstrickland@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2028',
+    'GPA': 'TBD'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1551", "MATH 1554", "GT 1000", "ENGL 1101"], 
+    "names":["Intro to CS", "Differential Calculus", "Linear Algebra", "First-Year Seminar", "English Composition 1"], 
+    "semester_taken":["1st", "1st", "1st", "1st", "1st"],
+    "skills":["Coding in python and understanding how to think like a programer", "Taking the derivative of a function", "Understanding and using matricies to understand data", "Understanding Georgia Tech as a first year", "Writing complex essays using rhetorical skills"],
+    }
+experience_data = {
+    "Cashier at WNB Factory": (["- Helped customers with their orders",
+                                                                          "- Maintained a clean eating enviornment", "- Restocked Condiments"],"Images/WNB.jpg"),
+    
+}
+
+projects_data = {
+    "Duchenne Muscular Dystrophy Hand Gadget": "Created a gadget to build motor skills and hand muscles for those with DMD",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 25,
+    "C": 10,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"English": "💂",
+    "Spanish":"💃🏽"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Partially Fluent",
+}
+leadership_data = {
+    "Georgia Tech Hult Prize Campus Director": (["- Elected to organize Hult Prize events"],"Images/Hult.jpg"),
+
+}
+activity_data={
+    "Soup Kitchen Volunteer": ["- Serve warm soup", 
+            "- Make various kinds of soup"]
+}
